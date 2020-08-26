1cf6a95969ea4cc890306f9d5071e8f4639c0e21
Mateus <mateus@dotse.com.br>
2020-08-26 13:56:21 -0300

    arrumado problema das duplicatas

--- src/screens/Jornada/JornadaLancamento/JornadaLancamento.js
+++ src/screens/Jornada/JornadaLancamento/JornadaLancamento.js
@@ -20,8 +20,8 @@ function CardContent({
   veiculo,
   data,
   hora
-}){
-  return(
+}) {
+  return (
     <View>
       <Text>Motorista: {motorista}</Text>
       <Text>Veículo: {veiculo}</Text>
@@ -33,137 +33,138 @@ function CardContent({
 }
 
 function JornadaLancamento(props) {
-    const [values, onChange] = React.useState({
-      obs:        '',
-      data:       moment().format('DD/MM/YYYY'),
-      hora:       moment().format('HH:mm'),
-      full_date:  new Date()
-    });
+  const [values, onChange] = React.useState({
+    obs: '',
+    data: moment().format('DD/MM/YYYY'),
+    hora: moment().format('HH:mm'),
+    full_date: new Date()
+  });
 
-    const [savedModal, onChangeSavedModal] = React.useState(false);
-    const [errorModal, onChangeErrorModal] = React.useState(false);
-    const [loader, setLoader] = React.useState(false);
+  const [savedModal, onChangeSavedModal] = React.useState(false);
+  const [errorModal, onChangeErrorModal] = React.useState(false);
+  const [loader, setLoader] = React.useState(false);
 
-    const onTextFieldChange = (value, name) => {
-      onChange({
-        ...values,
-        [name] : value
-      })
-    }
-
-
-    React.useEffect(() => {
-      GeolocationController.index()
-    }, []);
+  const onTextFieldChange = (value, name) => {
+    onChange({
+      ...values,
+      [name]: value
+    })
+  }
 
 
-    function storeLancamento(){
-      console.log(props.user)
+  React.useEffect(() => {
+    GeolocationController.index()
+  }, []);
+
+
+  async function storeLancamento() {
+    try {
       setLoader(true)
       let data = {
-        latitude:       props.geolocation && props.geolocation.coords && props.geolocation.coords.latitude ? props.geolocation.coords.latitude : 0,
-        longitude:      props.geolocation && props.geolocation.coords && props.geolocation.coords.longitude ? props.geolocation.coords.longitude : 0,
-        data:           values.full_date,
-        user_id:        props.user && props.user.user && props.user.user.id,
-        empresa_id:     props.user && props.user.user && props.user.user.empresa && props.user.user.empresa.id,
-        user_nome:      props.user && props.user.user && props.user.user.name,
-        veiculo_nome:   props.user && props.user.user && props.user.user.veiculo && props.user.user.veiculo.veiculo && props.user.user.veiculo.veiculo.placa ? props.user.user.veiculo.veiculo.placa : 'Sem Placa',
-        veiculo_id:     props.user && props.user.user && props.user.user.veiculo && props.user.user.veiculo.id,
-        obs:            values.obs,
-        status:         3,
-        tipo_id:        props.navigation.state.params && props.navigation.state.params.id,        
-        descricao:      props.navigation.state.params && props.navigation.state.params.descricao        
+        latitude: props.geolocation && props.geolocation.coords && props.geolocation.coords.latitude ? props.geolocation.coords.latitude : 0,
+        longitude: props.geolocation && props.geolocation.coords && props.geolocation.coords.longitude ? props.geolocation.coords.longitude : 0,
+        data: values.full_date,
+        user_id: props.user && props.user.user && props.user.user.id,
+        empresa_id: props.user && props.user.user && props.user.user.empresa && props.user.user.empresa.id,
+        user_nome: props.user && props.user.user && props.user.user.name,
+        veiculo_nome: props.user && props.user.user && props.user.user.veiculo && props.user.user.veiculo.veiculo && props.user.user.veiculo.veiculo.placa ? props.user.user.veiculo.veiculo.placa : 'Sem Placa',
+        veiculo_id: props.user && props.user.user && props.user.user.veiculo && props.user.user.veiculo.id,
+        obs: values.obs,
+        status: 3,
+        tipo_id: props.navigation.state.params && props.navigation.state.params.id,
+        descricao: props.navigation.state.params && props.navigation.state.params.descricao
       }
-      console.log(data)
-      JornadaController.store(data).then((res) => {
-        console.log(res)
-        setLoader(false)
-        onChangeSavedModal(true)
-      })
-      .catch(err => {
-        console.log(err)
-        onChangeErrorModal(true)
-        setLoader(false)
-      })
+
+      await JornadaController.store(data)
+      onChangeSavedModal(true)
+
+    }
+    catch (err) {
+      onChangeErrorModal(true)
+    }
+    finally {
+      setLoader(false)
     }
 
-    function onCloseSaveModal(){
-      onChangeSavedModal(false)
-      props.navigation.goBack()
-    }
-    
-    function onCloseErrorModal(){
-      onChangeErrorModal(false)
-    }
+  }
 
-    
-    return (
-      <Container>
-        <CustomHeader title={props.navigation.state.params && props.navigation.state.params.descricao} navigation={props} back/>
-        <Content contentContainerStyle={styles.content}>
+  function onCloseSaveModal() {
+    onChangeSavedModal(false)
+    props.navigation.goBack()
+  }
 
-          <CustomCard
-            title={"Evento: " + (props.navigation.state.params ? props.navigation.state.params.descricao : '')}
-            content={
-              <CardContent
-                motorista={props.user && props.user.user && props.user.user.name}
-                veiculo={props.user && props.user.user && props.user.user.veiculo && props.user.user.veiculo.veiculo && props.user.user.veiculo.veiculo.placa ? props.user.user.veiculo.veiculo.placa : 'Sem Placa'}
-                data={values.data}
-                hora={values.hora}
-              />
-            }
-          />
+  function onCloseErrorModal() {
+    onChangeErrorModal(false)
+  }
 
-          <MaterialTextField
-            label='Observação'
-            value={values.obs}
-            inputContainerPadding={30}
-            multiline
-            placeholder="Digite aqui..."
-            onChangeText={ (value) =>  onTextFieldChange(value, 'obs')}
-          />
 
-          <Grid style={{marginTop: 20}}>
-            <Col style={{paddingRight: 5}}>
-              <CustomButton
-                text="Cancelar"
-                type="cancel"
-                onPress={() => props.navigation.goBack()}
-              />
-            </Col>
-            <Col style={{paddingLeft: 5}}>
-              <CustomButton
-                text="Salvar"
-                onPress={storeLancamento}
-              />
-            </Col>
-          </Grid>
-          <Modal
-            isVisible={savedModal}
-            onClose={onCloseSaveModal}
-            text="Lançamento realizado com sucesso!"
-            type={'sucess'}
-          />
-          <Modal
-            isVisible={errorModal}
-            onClose={onCloseErrorModal}
-            text="O lançamento não pode ser realizado..."
-            type={'error'}
-          />
-          <Loader isVisible={loader}/>
+  return (
+    <Container>
+      <CustomHeader title={props.navigation.state.params && props.navigation.state.params.descricao} navigation={props} back />
+      <Content contentContainerStyle={styles.content}>
 
-        </Content>
-        
-      </Container>
-    );
-  
+        <CustomCard
+          title={"Evento: " + (props.navigation.state.params ? props.navigation.state.params.descricao : '')}
+          content={
+            <CardContent
+              motorista={props.user && props.user.user && props.user.user.name}
+              veiculo={props.user && props.user.user && props.user.user.veiculo && props.user.user.veiculo.veiculo && props.user.user.veiculo.veiculo.placa ? props.user.user.veiculo.veiculo.placa : 'Sem Placa'}
+              data={values.data}
+              hora={values.hora}
+            />
+          }
+        />
+
+        <MaterialTextField
+          label='Observação'
+          value={values.obs}
+          inputContainerPadding={30}
+          multiline
+          placeholder="Digite aqui..."
+          onChangeText={(value) => onTextFieldChange(value, 'obs')}
+        />
+
+        <Grid style={{ marginTop: 20 }}>
+          <Col style={{ paddingRight: 5 }}>
+            <CustomButton
+              text="Cancelar"
+              type="cancel"
+              onPress={() => props.navigation.goBack()}
+            />
+          </Col>
+          <Col style={{ paddingLeft: 5 }}>
+            <CustomButton
+              text="Salvar"
+              onPress={storeLancamento}
+            />
+          </Col>
+        </Grid>
+        <Modal
+          isVisible={savedModal}
+          onClose={onCloseSaveModal}
+          text="Lançamento realizado com sucesso!"
+          type={'sucess'}
+        />
+        <Modal
+          isVisible={errorModal}
+          onClose={onCloseErrorModal}
+          text="O lançamento não pode ser realizado..."
+          type={'error'}
+        />
+        <Loader isVisible={loader} />
+
+      </Content>
+
+    </Container>
+  );
+
 }
 
 
 
 const mapStateToProps = store => ({
-  user:         store.user.user,
-  geolocation:  store.geolocation.geolocation
+  user: store.user.user,
+  geolocation: store.geolocation.geolocation
 });
 
 export default connect(mapStateToProps, null)(JornadaLancamento);

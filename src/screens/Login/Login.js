6e6389924f9ee80be6ea43db134e760d9c911f13
Rene Soares <renesoares@MacBook-Pro.local>
2020-10-27 22:38:27 -0300

    fix: colocado os stops dos loads nós fluxos que estavam faltando.

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,18 +1,39 @@
-import { isEmpty, isNull, isUndefined } from 'lodash';
-import { Button, Container, Content, Form, Icon, Input, Item, Text, Toast, Grid, ListItem, Body } from 'native-base';
-import React, { Component } from 'react';
-import { BackHandler, Dimensions, Image, ImageBackground, Platform, StatusBar, StyleSheet, View } from 'react-native';
+import {isEmpty, isNull, isUndefined} from 'lodash';
+import {
+  Button,
+  Container,
+  Content,
+  Form,
+  Icon,
+  Input,
+  Item,
+  Text,
+  Toast,
+  Grid,
+  ListItem,
+  Body,
+} from 'native-base';
+import React, {Component} from 'react';
+import {
+  BackHandler,
+  Dimensions,
+  Image,
+  ImageBackground,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
 import Loader from '../../components/Loader';
 import AuthController from '../../controllers/AuthController';
-import { colors } from '../../theme/colors';
+import {colors} from '../../theme/colors';
 import PasswordRecovery from './PasswordRecovery';
-import { images } from './styles';
+import {images} from './styles';
 import CheckBox from '@react-native-community/checkbox';
 import CustomCheckBox from '../../components/CustomCheckBox';
 import AsyncStorage from '@react-native-community/async-storage';
 import SecuryStorageController from '../../controllers/SecuryStorageController';
 import OneTapLogin from './OneTapLogin';
-
 
 class Login extends Component {
   constructor(props) {
@@ -23,65 +44,74 @@ class Login extends Component {
       loader: false,
       remember: false,
       passwordRecoveryModal: false,
-      oneTapLogin: false
+      oneTapLogin: false,
     };
     this.handleBackButtonClick = this.handleBackButtonClick.bind(this);
   }
 
-
   componentWillMount() {
-    const { navigation } = this.props;
-    this.getStoreCredential()
-    BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonClick);
-    this.focusListener = navigation.addListener("didBlur", () => {
-      BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonClick);
+    const {navigation} = this.props;
+    this.getStoreCredential();
+    BackHandler.addEventListener(
+      'hardwareBackPress',
+      this.handleBackButtonClick,
+    );
+    this.focusListener = navigation.addListener('didBlur', () => {
+      BackHandler.removeEventListener(
+        'hardwareBackPress',
+        this.handleBackButtonClick,
+      );
     });
-    this.focusListener = navigation.addListener("didFocus", () => {
-      BackHandler.addEventListener('hardwareBackPress', this.handleBackButtonClick);
+    this.focusListener = navigation.addListener('didFocus', () => {
+      BackHandler.addEventListener(
+        'hardwareBackPress',
+        this.handleBackButtonClick,
+      );
       this.setState({
         username: '',
         password: '',
-      })
+      });
     });
   }
 
   async getStoreCredential() {
     try {
-      const value = await AsyncStorage.getItem('remember_credentials')
+      const value = await AsyncStorage.getItem('remember_credentials');
       if (value !== null && value != 'false') {
-        const credentials = await SecuryStorageController.get()
+        const credentials = await SecuryStorageController.get();
 
-        console.log(credentials)
+        console.log(credentials);
         this.setState({
           ...credentials,
           remember: true,
-          oneTapLogin: credentials.password ? true : false
-        })
+          oneTapLogin: credentials.password ? true : false,
+        });
       }
-    }
-    catch (err) {
-      console.log(err)
+    } catch (err) {
+      console.log(err);
     }
   }
 
   async settStoreCredential(value) {
     try {
-      await AsyncStorage.setItem('remember_credentials', value)
-      return Promise.resolve('done')
-    }
-    catch (err) {
-      console.log(err)
-      return Promise.resolve(err)
+      await AsyncStorage.setItem('remember_credentials', value);
+      return Promise.resolve('done');
+    } catch (err) {
+      console.log(err);
+      return Promise.resolve(err);
     }
   }
 
   componentWillUnmount() {
-    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButtonClick);
+    BackHandler.removeEventListener(
+      'hardwareBackPress',
+      this.handleBackButtonClick,
+    );
     this.focusListener.remove();
   }
 
   handleBackButtonClick() {
-    BackHandler.exitApp()
+    BackHandler.exitApp();
     return true;
   }
 
@@ -90,160 +120,176 @@ class Login extends Component {
       Toast.show({
         text: 'Usário e senha não podem ser vazios!',
         type: 'warning',
-        duration: 3000
-      })
-      return false
+        duration: 3000,
+      });
+      return false;
     }
-    return true
-
-  }
-
+    return true;
+  };
 
   goHome = () => {
-    this.props.navigation.navigate('Home', { updade: true })
-  }
+    this.props.navigation.navigate('Home', {updade: true});
+  };
 
-
-  hasFields = (field) => {
-    return isNull(field) || isUndefined(field) || isEmpty(field)
+  hasFields = field => {
+    return isNull(field) || isUndefined(field) || isEmpty(field);
   };
 
   login() {
     if (!this.validations()) {
-      return
+      return;
     }
-    this.setState({ loader: true })
+    this.setState({loader: true});
 
     AuthController.index({
       username: this.state.username,
-      password: this.state.password
-    }).then((res) => {
-      this.settStoreCredential(this.state.remember ? 'true' : 'false').then(() => {
-        this.setState({ loader: false })
-        this.props.navigation.navigate('Home')
-      })
+      password: this.state.password,
     })
-      .catch(err => {
-        console.log(err)
-        this.setState({ loader: false })
+      .then(res => {
+        this.settStoreCredential(this.state.remember ? 'true' : 'false').then(
+          () => {
+            this.setState({loader: false});
+            this.props.navigation.navigate('Home');
+          },
+        );
       })
-  };
+      .catch(err => {
+        console.log(err);
+        this.setState({loader: false});
+      });
+  }
 
   oneTapAuthLogin = async () => {
-    try{
-      this.setState({ loader: true })
-      await AuthController.oneTapLogin()
-      this.props.navigation.navigate('Home')
-    }
-    catch(err){
+    try {
+      this.setState({loader: true});
+      await AuthController.oneTapLogin();
+      this.setState({loader: false});
+      this.props.navigation.navigate('Home');
+    } catch (err) {
+      this.setState({loader: false});
       Toast.show({
-        text: 'Algo deu errado, aperte em entrar com outra conta e redigite seu usuário e senha.',
-        type: 'warning'
-      })
+        text:
+          'Algo deu errado, aperte em entrar com outra conta e redigite seu usuário e senha.',
+        type: 'warning',
+      });
+    } finally {
+      this.setState({loader: false});
     }
-    finally{
-      this.setState({ loader: false })
-    }
-  }
+  };
 
   anotherAccount = async () => {
-    try{
-      await SecuryStorageController.reset()
-      await AuthController.cleanUserData()
+    try {
+      await SecuryStorageController.reset();
+      await AuthController.cleanUserData();
       this.setState({
         oneTapLogin: false,
-        password: ''
-      })
+        password: '',
+      });
+    } catch (err) {
+      console.log(err);
     }
-    catch(err){
-      console.log(err)
-    }
-  }
+  };
 
   render() {
     return (
       <Container style={styles.container}>
-        <StatusBar backgroundColor={colors.primary.main} barStyle={Platform.OS === 'ios' ? "dark-content" : "light-content"} />
+        <StatusBar
+          backgroundColor={colors.primary.main}
+          barStyle={Platform.OS === 'ios' ? 'dark-content' : 'light-content'}
+        />
         <Loader isVisible={this.state.loader} />
-        <ImageBackground source={images.background} style={{ width: '100%', height: '100%' }}>
-          <View style={{ width: '100%', height: '100%' }}>
+        <ImageBackground
+          source={images.background}
+          style={{width: '100%', height: '100%'}}>
+          <View style={{width: '100%', height: '100%'}}>
             <Content
               contentContainerStyle={{
                 flex: 1,
                 alignItens: 'center',
                 justifyContent: 'center',
-                flexDirection: 'column'
+                flexDirection: 'column',
               }}>
-              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+              <View style={{justifyContent: 'center', alignItems: 'center'}}>
                 <Image
                   source={require('../../images/logo_transporter.png')}
                   style={{
                     width: Dimensions.get('window').width * 0.6,
                     height: Dimensions.get('window').width * 0.5,
-                    resizeMode: 'contain'
+                    resizeMode: 'contain',
                   }}
                 />
               </View>
 
-              {
-                this.state.oneTapLogin ? 
-                  <OneTapLogin
-                      onLeave={this.anotherAccount} 
-                      username={this.state.username}
-                      password={this.state.password}     
-                      onLogin={this.oneTapAuthLogin}  
-                  /> 
-                  :
-                  <View style={styles.loginContainer}>
-                    <Form>
-                      <Item style={styles.Item}>
-                        <Icon style={styles.icon} type='FontAwesome' name='user' />
-                        <Input
-                          style={styles.input}
-                          placeholder='Usuário'
-                          value={this.state.username}
-                          autoCapitalize='none'
-                          onChangeText={username => {
-                            this.setState({ username })
-                          }}
-                        />
-                      </Item>
-                      <Item style={{ ...styles.Item, marginTop: 10 }}>
-                        <Icon style={styles.icon} type='FontAwesome' name='lock' />
-                        <Input
-                          style={styles.input}
-                          placeholder='Senha'
-                          value={this.state.password}
-                          secureTextEntry={true}
-                          onChangeText={password => {
-                            this.setState({ password })
-                          }}
-                        />
-                      </Item>
-                      <CustomCheckBox
-                        label="Lembre-se"
-                        checked={this.state.remember}
-                        onValueChange={(remember) => {
-                          this.setState({ remember: remember })
+              {this.state.oneTapLogin ? (
+                <OneTapLogin
+                  onLeave={this.anotherAccount}
+                  username={this.state.username}
+                  password={this.state.password}
+                  onLogin={this.oneTapAuthLogin}
+                />
+              ) : (
+                <View style={styles.loginContainer}>
+                  <Form>
+                    <Item style={styles.Item}>
+                      <Icon
+                        style={styles.icon}
+                        type="FontAwesome"
+                        name="user"
+                      />
+                      <Input
+                        style={styles.input}
+                        placeholder="Usuário"
+                        value={this.state.username}
+                        autoCapitalize="none"
+                        onChangeText={username => {
+                          this.setState({username});
                         }}
                       />
-                      {/* <View style={{display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-end', marginTop: 10}}>
+                    </Item>
+                    <Item style={{...styles.Item, marginTop: 10}}>
+                      <Icon
+                        style={styles.icon}
+                        type="FontAwesome"
+                        name="lock"
+                      />
+                      <Input
+                        style={styles.input}
+                        placeholder="Senha"
+                        value={this.state.password}
+                        secureTextEntry={true}
+                        onChangeText={password => {
+                          this.setState({password});
+                        }}
+                      />
+                    </Item>
+                    <CustomCheckBox
+                      label="Lembre-se"
+                      checked={this.state.remember}
+                      onValueChange={remember => {
+                        this.setState({remember: remember});
+                      }}
+                    />
+                    {/* <View style={{display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-end', marginTop: 10}}>
                     <Text onPress={()=> this.setState({passwordRecoveryModal: true})}>Esqueci minha senha</Text>
                   </View> */}
-                      <Item style={styles.ItemButton}>
-                        <Button style={styles.button} onPress={() => { this.login() }}>
-                          <Text style={styles.buttonText}>ENTRAR</Text>
-                        </Button>
-                      </Item>
-                    </Form>
-                  </View>
-              }
+                    <Item style={styles.ItemButton}>
+                      <Button
+                        style={styles.button}
+                        onPress={() => {
+                          this.login();
+                        }}>
+                        <Text style={styles.buttonText}>ENTRAR</Text>
+                      </Button>
+                    </Item>
+                  </Form>
+                </View>
+              )}
             </Content>
           </View>
         </ImageBackground>
         <PasswordRecovery
           open={this.state.passwordRecoveryModal}
-          onClose={() => this.setState({ passwordRecoveryModal: false })}
+          onClose={() => this.setState({passwordRecoveryModal: false})}
         />
       </Container>
     );
@@ -251,7 +297,6 @@ class Login extends Component {
 }
 
 export default Login;
-
 
 const styles = StyleSheet.create({
   loginContainer: {
@@ -262,7 +307,7 @@ const styles = StyleSheet.create({
     margin: Dimensions.get('window').width * 0.08,
     paddingRight: 14,
     paddingTop: 15,
-    paddingBottom: 20
+    paddingBottom: 20,
   },
   button: {
     borderRadius: 7,
@@ -270,21 +315,20 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     justifyContent: 'center',
     alignItems: 'center',
-    width: '100%'
+    width: '100%',
   },
   icon: {
     color: colors.primary.main,
   },
   text: {
     color: '#757575',
-    fontSize: 12
+    fontSize: 12,
   },
   ItemButton: {
     borderColor: 'transparent',
     marginTop: 0,
   },
   Item: {
-    borderBottomColor: colors.primary.main
-  }
+    borderBottomColor: colors.primary.main,
+  },
 });
-
--- src/screens/Relatorios/Lancamentos/Lancamentos.js
+++ src/screens/Relatorios/Lancamentos/Lancamentos.js
@@ -1,140 +1,159 @@
-import { Container, Text, Content, Button, ListItem, Icon, View} from 'native-base';
+import {Container, Text, Icon, View} from 'native-base';
 import React from 'react';
 import CustomHeader from '../../../components/CustomHeader';
-import { styles, images } from './styles';
-import { connect } from 'react-redux';
-import { FlatList, ScrollView } from 'react-native';
+import {styles, images} from './styles';
+import {connect} from 'react-redux';
+import {FlatList, ScrollView} from 'react-native';
 import {isEmpty, isUndefined} from 'lodash';
 import JornadaController from '../../../controllers/JornadaController';
 import moment from 'moment';
 import LancamentoItem from './LancamentoItem';
 import LancamentoFilter from './LancamentoFilter';
-import { colors } from '../../../theme/colors';
+import {colors} from '../../../theme/colors';
 import InlineLoader from '../../../components/InlineLoader';
-import Loader from '../../../components/Loader';
 
+function Lancamentos(props) {
+  const [collapse, onCollapse] = React.useState(true);
+  const [loader, setLoader] = React.useState(true);
 
-function Lancamentos(props){
-    const [collapse, onCollapse] = React.useState(true)
-    const [loader, setLoader] = React.useState(true)
+  const [period, onPeriodChange] = React.useState({
+    startDate: moment().subtract(30, 'days'),
+    endDate: moment().add(2, 'days'),
+  });
 
-    const [period, onPeriodChange] = React.useState({
-        startDate:  moment().subtract(30, 'days'),
-        endDate:    moment().add(2, 'days')
-    })
+  React.useEffect(() => {
+    getLancamentos(period.startDate, period.endDate);
+  }, [period.endDate, period.startDate]);
 
-    React.useEffect(() => {
-        getLancamentos(period.startDate, period.endDate)
-    }, []);
+  function getLancamentos(startDate, endDate) {
+    setLoader(true);
+    setTimeout(() => {
+      if (moment().diff(moment(startDate), 'days') >= 59) {
+        console.log('teste');
+        JornadaController.getLancamentosAPI(
+          moment().diff(moment(startDate), 'days'),
+        )
+          .then(() => {
+            setLoader(false);
+          })
+          .catch(err => {
+            setLoader(false);
+            console.log(err);
+          });
+      } else {
+        JornadaController.getLancamentos(
+          'data > ' +
+            moment(startDate).format('YYYY-MM-DD@HH:mm:ss') +
+            ' AND ' +
+            'data < ' +
+            moment(endDate).format('YYYY-MM-DD@HH:mm:ss'),
+        )
+          .then(() => {
+            setLoader(false);
+          })
+          .catch(() => {
+            setLoader(false);
+          });
+      }
+    }, 500);
+  }
 
-    function getLancamentos(startDate, endDate){
-        setLoader(true)
-        setTimeout(() => {
-            if(moment().diff(moment(startDate), 'days') >= 59){
-                console.log('teste')
-                JornadaController.getLancamentosAPI(moment().diff(moment(startDate), 'days')).then(()=>{
-                    setLoader(false)
-                })
-                .catch(err => {
-                    setLoader(false)
-                    console.log(err)
-                })
-            }
-            else{
-                JornadaController.getLancamentos(
-                    'data > ' + moment(startDate).format('YYYY-MM-DD@HH:mm:ss') +
-                    ' AND ' +
-                    'data < ' + moment(endDate).format('YYYY-MM-DD@HH:mm:ss')
-                ).then(() => {
-                    setLoader(false)
-                })
-                .catch(() => {
-                    setLoader(false)
-                })
-            }
-        }, 500)
-
+  async function syncWithApi(startDate, endDate) {
+    try {
+      setLoader(true);
+      await JornadaController.syncLancamentosEnviar();
+      await JornadaController.syncLancamentosApi();
+      await JornadaController.getLancamentos(
+        'data > ' +
+          moment(startDate).format('YYYY-MM-DD@HH:mm:ss') +
+          ' AND ' +
+          'data < ' +
+          moment(endDate).format('YYYY-MM-DD@HH:mm:ss'),
+      );
+      setLoader(false);
+    } catch (err) {
+      setLoader(false);
+      console.log(err);
     }
+  }
 
-    async function syncWithApi(startDate, endDate){
-        try{
-            setLoader(true)
-            await JornadaController.syncLancamentosEnviar()
-            await JornadaController.syncLancamentosApi()
-            await JornadaController.getLancamentos(
-                'data > ' + moment(startDate).format('YYYY-MM-DD@HH:mm:ss') +
-                ' AND ' +
-                'data < ' + moment(endDate).format('YYYY-MM-DD@HH:mm:ss')
-            )
-            setLoader(false)
+  function onFilterChange(value, name) {
+    setLoader(true);
+    onPeriodChange({
+      ...period,
+      [name]: moment(value),
+    });
+    if (name === 'startDate') {
+      getLancamentos(moment(value), period.endDate);
+    } else {
+      getLancamentos(period.startDate, moment(value));
+    }
+    setLoader(false);
+  }
+
+  const renderItem = ({item, index}) => (
+    <LancamentoItem item={item} index={index} lancamentos={props.lancamentos} />
+  );
+  const keyExtractor = (item, index) =>
+    'jornada_lancamento_item' + index + item.data;
+  const emptyComponent = () => (
+    <View style={styles.emptyContainer}>
+      <Text>Sem lançamentos para esse período.</Text>
+      <Icon
+        type="FontAwesome"
+        name="ellipsis-h"
+        style={{color: colors.secondary.main}}
+      />
+    </View>
+  );
+
+  return (
+    <Container>
+      <CustomHeader
+        title="LANÇAMENTOS"
+        navigation={props}
+        icon={
+          <Icon
+            type="MaterialIcons"
+            name="sync"
+            onPress={() => syncWithApi(period.startDate, period.endDate)}
+            style={{
+              color: 'white',
+            }}
+          />
         }
-        catch(err){
-            setLoader(false)
-            console.log(err)
-        }
-    }
-
-    function onFilterChange(value, name){
-        setLoader(true)
-        onPeriodChange({
-            ...period,
-            [name] : moment(value)
-        })
-        if(name === 'startDate')
-            getLancamentos(moment(value), period.endDate)
-        else getLancamentos(period.startDate, moment(value))
-    }
-    
-    const renderItem = ({item, index}) => <LancamentoItem item={item} index={index} lancamentos={props.lancamentos}/>
-    const keyExtractor = (item, index) =>  'jornada_lancamento_item' + index + item.data;
-    const emptyComponent = () =>  (
-        <View style={styles.emptyContainer}>
-            <Text>Sem lançamentos para esse período.</Text>
-            <Icon type="FontAwesome" name="ellipsis-h" style={{color: colors.secondary.main}}/>
-        </View>
-    )
-    
-    return(
-        <Container>
-            <CustomHeader 
-                title="LANÇAMENTOS" 
-                navigation={props} 
-                icon={
-                    <Icon 
-                        type="MaterialIcons" 
-                        name="sync"
-                        onPress={() => syncWithApi(period.startDate, period.endDate)} 
-                        style={{
-                            color: 'white'
-                        }}
-                    />
-                }
-            />
-            <ScrollView stickyHeaderIndices={[0]}>
-                <LancamentoFilter 
-                    isCollapsed={collapse} 
-                    onCollapse={() => onCollapse(!collapse)}
-                    onChange={onFilterChange}
-                    period={period} 
-                />
-                {loader && !isEmpty(props.lancamentos) && !isEmpty(props.lancamentos.list) && props.lancamentos.list.length > 7 ? <InlineLoader isVisible={loader} dense/> : null}
-                <FlatList
-                   data={!isEmpty(props.lancamentos) && !isEmpty(props.lancamentos.list) ? props.lancamentos.list : []}
-                   keyExtractor={keyExtractor}
-                   renderItem={renderItem}
-                   ListEmptyComponent={!loader ? emptyComponent : null}  
-                />
-                {loader ? <InlineLoader isVisible={loader}/> : null}
-            </ScrollView>
-        </Container>
-    )
+      />
+      <ScrollView stickyHeaderIndices={[0]}>
+        <LancamentoFilter
+          isCollapsed={collapse}
+          onCollapse={() => onCollapse(!collapse)}
+          onChange={onFilterChange}
+          period={period}
+        />
+        {loader &&
+        !isEmpty(props.lancamentos) &&
+        !isEmpty(props.lancamentos.list) &&
+        props.lancamentos.list.length > 7 ? (
+          <InlineLoader isVisible={loader} dense />
+        ) : null}
+        <FlatList
+          data={
+            !isEmpty(props.lancamentos) && !isEmpty(props.lancamentos.list)
+              ? props.lancamentos.list
+              : []
+          }
+          keyExtractor={keyExtractor}
+          renderItem={renderItem}
+          ListEmptyComponent={!loader ? emptyComponent : null}
+        />
+        {loader ? <InlineLoader isVisible={loader} /> : null}
+      </ScrollView>
+    </Container>
+  );
 }
 
-
 const mapStateToProps = store => ({
-    lancamentos: store.lancamentos.lancamentos
-  });  
-  
-  
-  export default connect(mapStateToProps)(Lancamentos);
-  
+  lancamentos: store.lancamentos.lancamentos,
+});
+
+export default connect(mapStateToProps)(Lancamentos);

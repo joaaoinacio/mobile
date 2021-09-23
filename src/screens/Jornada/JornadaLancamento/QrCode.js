28e4f11ade1df72325d42fe1b903667ba4eb501f
Mateus <mateus@dotse.com.br>
2021-09-23 11:24:42 -0300

    Ajustes para rodar a lib do qr code

--- src/screens/Jornada/JornadaLancamento/QrCode.js
+++ src/screens/Jornada/JornadaLancamento/QrCode.js
@@ -1,46 +1,41 @@
-'use strict';
-
 import React, { Component } from 'react';
-
 import {
-  AppRegistry,
-  StyleSheet,
+  Linking, StyleSheet,
   Text,
-  TouchableOpacity,
-  Linking
+  TouchableOpacity
 } from 'react-native';
-
-import QRCodeScanner from 'react-native-qrcode-scanner';
 import { RNCamera } from 'react-native-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
-class ScanScreen extends Component {
-  onSuccess = e => {
+function ScanScreen(props) {
+
+  const onSuccess = e => {
     Linking.openURL(e.data).catch(err =>
       console.error('An error occured', err)
     );
   };
 
-  render() {
-    return (
-      <QRCodeScanner
-        onRead={this.onSuccess}
-        flashMode={RNCamera.Constants.FlashMode.torch}
-        topContent={
-          <Text style={styles.centerText}>
-            Go to{' '}
-            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-            your computer and scan the QR code.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
-      />
-    );
-  }
+  return (
+    <QRCodeScanner
+      onRead={onSuccess}
+      flashMode={RNCamera.Constants.FlashMode.torch}
+      topContent={
+        <Text style={styles.centerText}>
+          Go to{' '}
+          <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+          your computer and scan the QR code.
+        </Text>
+      }
+      bottomContent={
+        <TouchableOpacity style={styles.buttonTouchable}>
+          <Text style={styles.buttonText}>OK. Got it!</Text>
+        </TouchableOpacity>
+      }
+    />
+  );
+
 }
+
 
 const styles = StyleSheet.create({
   centerText: {
@@ -62,4 +57,5 @@ const styles = StyleSheet.create({
   }
 });
 
-AppRegistry.registerComponent('default', () => ScanScreen);
+
+export default ScanScreen;
